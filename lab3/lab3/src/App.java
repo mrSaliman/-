9c3e9101d6f6b4dae70b50b1372fcded077b3dd5
mrSaliman <sloi.25abc@gmail.com>
2023-03-31 11:41:14 +0300

    lr3

---
+++ lab3/lab3/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] arr = {5, 2, 9, 1, 5, 6, 8, 2, 1, 5, 12, 14, 22};
+        CountingSort.sort(arr, 22);
+        System.out.println(Arrays.toString(arr));
+    }
+}
